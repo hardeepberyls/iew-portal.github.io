72aa499cd58609103c3db6e29a812c5b1d3d6a9a
hardeepberyls <hardeepbhimra1992@gmail.com>
2023-05-03 11:00:17 +0530

    database query updated

--- server/model/Mymodal.js
+++ server/model/Mymodal.js
@@ -3,4 +3,6 @@ const bodyparser = require("body-parser");
 const app = express();
 const dbase= require("../config/database")
 
-dbase.query("insert into sale where ")
+dbase.query("insert into sale where 'invoice_no','date','e_way','transport','po_number','po_date','challan_no','challan_date','name','gst_no','address','city','state','pincode','serial_number','item_code','item_description','hsn_code','quantity','uom','rate','amount','sgst','cgst','igst','amount' values ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?")
+
+
